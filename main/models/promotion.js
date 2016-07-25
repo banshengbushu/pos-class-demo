a86a2f7eb2b672b4b9b9fb5328e8f4ebd9923b96
banshengbushu <wangdanna1995@outlook.com>
2016-07-25 20:54:48 +0800

    replace promoton object by item instance

---
+++ main/models/promotion.js
@@ -0,0 +1,11 @@
+/**
+ * Created by wangdanna on 16-7-25.
+ */
+
+class Promotions{
+    constructor(type,barcodes = []){
+        this.type = type;
+        this.barcodes = barcodes;
+    }
+}
+module.exports = Promotions;
