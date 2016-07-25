089ac7f2c544aceedd76a26ed318551e42629cce
banshengbushu <wangdanna1995@outlook.com>
2016-07-25 21:17:51 +0800

    create CartItem class

---
+++ main/models/cart-items.js
@@ -0,0 +1,10 @@
+/**
+ * Created by wangdanna on 16-7-25.
+ */
+class CartItem {
+    constructor(item,count=1){
+        this.item = item;
+        this.count = count;
+    }
+}
+module.exports = CartItem;
