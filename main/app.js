dd5f4e5bdd29b6e2d0bc851e07cfeae99f3e3985
banshengbushu <wangdanna1995@outlook.com>
2016-07-25 21:54:03 +0800

    move loadPromotions function to Promotion.all method

--- main/app.js
+++ main/app.js
@@ -1,19 +1,14 @@
-const fixture = require('./fixtures');
 const CartItem = require('./models/cart-items');
 const ReceiptItem = require('./models/receipt-items');
 const Receipt = require('./models/receipt');
 const Item = require('./models/item');
-const loadPromotions = fixture.loadPromotions;
-
-
-
+const Promotion = require('./models/promotion');
 
 function printReceipt(tags) {
     
   const cartItems = buildCartItems(tags, Item.all());
 
-  const allPromotions = loadPromotions();
-  const receiptItems = buildReceiptItems(cartItems, allPromotions);
+  const receiptItems = buildReceiptItems(cartItems, Promotion.all());
 
   const receipt = buildReceipt(receiptItems);
 
--- main/models/promotion.js
+++ main/models/promotion.js
@@ -1,11 +1,15 @@
 /**
  * Created by wangdanna on 16-7-25.
  */
-
 class Promotions{
     constructor(type,barcodes = []){
         this.type = type;
         this.barcodes = barcodes;
+    }
+    static all(){
+        return [
+            new Promotions('BUY_TWO_GET_ONE_FREE',['ITEM000000', 'ITEM000001', 'ITEM000005'])
+        ];
     }
 }
 module.exports = Promotions;
