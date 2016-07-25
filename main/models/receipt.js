a289917e66a6933453a22e3543db76d94a2b90a1
banshengbushu <wangdanna1995@outlook.com>
2016-07-26 00:33:20 +0800

    move buildReceiptText function to receipt.buildReceiptText method

--- main/models/receipt.js
+++ main/models/receipt.js
@@ -1,7 +1,7 @@
 /**
  * Created by wangdanna on 16-7-25.
  */
-class receipt{
+class Receipt{
     constructor(receiptItems, total, savedTotal){
         this.receiptItems = receiptItems;
         this.total = total;
@@ -9,9 +9,8 @@ class receipt{
     }
     static buildReceipt(receiptItems) {
 
-    let total = 0;
-    let savedTotal = 0;
-
+        let total = 0;
+        let savedTotal = 0;
     for (const receiptItem of receiptItems) {
         total += receiptItem.subtotal;
         savedTotal += receiptItem.saved;
@@ -19,6 +18,27 @@ class receipt{
 
     return new Receipt(receiptItems, total, savedTotal);
 }
+    buildReceiptText() {
+        const formatMoney = (money) => {
+            return money.toFixed(2);
+        };
 
+    let receiptItemsText = this.receiptItems
+    .map(receiptItem => {
+        const cartItem = receiptItem.cartItem;
+        return `名称：${cartItem.item.name}，\
+数量：${cartItem.count}${cartItem.item.unit}，\
+单价：${formatMoney(cartItem.item.price)}(元)，\
+小计：${formatMoney(receiptItem.subtotal)}(元)`;
+    })
+    .join('\n');
+
+    return `***<没钱赚商店>收据***
+${receiptItemsText}
+----------------------
+总计：${formatMoney(this.total)}(元)
+节省：${formatMoney(this.savedTotal)}(元)
+**********************`;
 }
-module.exports = receipt;
+}
+module.exports = Receipt;
